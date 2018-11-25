a74a5081ba1f7f2444bf946ffa5551c23853ce91
Luis Enrique Suarez <luissuarezdev@gmail.com>
2018-11-25 14:33:37 -0300

    socket

--- server.php
+++ server.php
@@ -1,16 +1,20 @@
 <?php
-
-    require_once 'vendor/autoload.php';
-
-    use Workerman\Worker;
-    use PHPSocketIO\SocketIO;
-
-    $io = new SocketIO(2021);
-
-    $io->on('connection', function ($socket) use ($io) {
-        $socket->on('print', function ($data) use ($io) {
-            $io->emit('print device ' . $data['id'], $data);
-        });
+  
+  require_once 'vendor/autoload.php';
+  
+  use Workerman\Worker;
+  use PHPSocketIO\SocketIO;
+  
+  $io = new SocketIO(2021);
+  
+  $io->on('connection', function ($socket) use ($io) {
+    $socket->on('print', function ($data) use ($io) {
+      $io->emit('print device ' . $data['id'], $data);
     });
-
-    Worker::runAll();
+    
+    $socket->on('emit', function ($data) use ($io) {
+      $io->emit($data['namespace'], $data['data'] ?? []);
+    });
+  });
+  
+  Worker::runAll();
